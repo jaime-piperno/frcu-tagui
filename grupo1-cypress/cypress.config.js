faf78d718df4ffb4111f90584c208459a436ac2b
Manuel <manuelriedel002@gmail.com>
2025-10-02 08:04:01 -0300

    Entrega Cypress Grupo 1

---
+++ grupo1-cypress/cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+<<<<<<< HEAD
+  projectId: '2nkww1',
+=======
+>>>>>>> 756fb1dbf89053bad341dfa7876d98ff89e0a643
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
